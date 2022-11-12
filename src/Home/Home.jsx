644ff9cfe4cc6f7dee487f6fb8ca321182e09117
Ba Trinh <trinhnguyenleba@gmail.com>
2022-11-12 15:33:46 +0700

    rm

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import FirstSection from './FirstSection'
+
 
 export default function Home() {
   return (
     <div>
-        <FirstSection />
+
     </div>
-   
   )
 }
--- src/ImgSrc.js
+++ src/ImgSrc.js
@@ -2,7 +2,22 @@ const imgSrc = {
     Home:{
         FirstSectionImg:{
             img1:"./asset/pic17.jpg"
+        },
+        ImageSection:{
+            img1:"./asset/pic8.jpg",
+            img2:"./asset/pic12.jpg",
+            img3:"./asset/pic13.jpg",
+            img4:"./asset/pic14.jpg",
+            img5:"./asset/pic15.jpg",
+        },
+        DetailSection:{
+            img1:"./asset/pic7.jpg"
+        },
+        MemberSection:{
+            avt1:"./asset/avt1.jpg",
+            avt2:"./asset/avt2.jpg",
+            avt3:"./asset/avt3.jpg"
         }
-    }
+    },
 }
 export default imgSrc;
